b9025879b37a56de20fcad8b0f0d3922e3f411fd
Alioune <alioune1117@gmail.com>
2020-06-10 16:57:09 +0200

    aggiunto la possibilità di disattivare dropdown al click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 $(document).ready(
   function(){
+    // Attivazione dropdown all'entrata del mouse
     $('.with_dropdown').mouseenter(
       function(){
         $(this).children('.dropdown').removeClass('hidden');
       }
     )
+    // Disattivazione dropdown all'uscita del mouse
     $('.with_dropdown').mouseleave(
+      function(){
+        $(this).children('.dropdown').addClass('hidden');
+      }
+    )
+    // Disattivazione dropdown al click
+    $('.with_dropdown').click(
       function(){
         $(this).children('.dropdown').addClass('hidden');
       }
